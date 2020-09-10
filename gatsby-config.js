0090400d7efd2a28e15658e2102a7b81e13a0720
alesin <alesintipler@gmail.com>
2020-09-10 05:39:03 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Visualize Code`,
+    author: `Tygrys`,
+    description: `A starter blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `gatsbyjs`,
